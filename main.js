1af3cef3df59312382bc82a3af3c678c8b792278
Fedor <uasily.lol@yandex.ru>
2022-03-21 09:34:44 +0300

    pract

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ span.onclick = openNav;
 let a = document.getElementById("close");
 a.onclick = closeNav;
 
+let losos = document.querySelector('.button'); // что - то с градиентом сделать
+losos.onclick = () => {
+    let world = document.getElementById('body');
+    let color = `linear-gradient(-45deg, rgb(${Math.floor(Math.random() * 255)}), rgb(${Math.floor(Math.random() * 255)}), rgb(${Math.floor(Math.random() * 255)}), rgb(${Math.floor(Math.random() * 255)}) )`;
+    // color = "linear-gradient(-45deg, #c0c0c0, #c0c0c0, #c0c0c0, #c0c0c0)";
+    debugger
+    // background: linear-gradient(#e66465, #9198e5);
+    document.body.style.backgroundImage;
+    // world.style.backgroundImage = ;
+    body.style.background = "linear-gradient(#e66465, rgb(231,122,11) )";
+    "linear-gradient(-45deg, #50352d, #d61818, #000000, #e6ff09)";
+   
+}
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -16,17 +29,18 @@ function closeNav() {
 let rds = document.querySelector('.rds-1');
 
 rds.onclick = () => {
-    rds.style.animationPlayState = "running";
+    rds.style.animationPlayState = "running";debugger
     setTimeout(bax, 5000);
 }
 
 let bax = () => {
+    debugger
     let world = document.querySelector('body');
     console.log();
-    world.innerHTML = '';
+    world.hidden = true;
     setTimeout(() => {
-        let world = document.querySelector('body');
-        world.style.background = "black"
-    }, 2000)
-
+          let world = document.querySelector('body');
+          world.hidden = false;
+          world.style.background = "#c0c0c0";
+          }, 4000);
 }
